86ca1f170964331ba5d076a1431d0bbbaab54167
Alexander <wurflalexander@gmail.com>
2026-07-07 19:49:21 +0200

    v3

--- src/main/java/com/thunderbear06/entity/player/AndroidPlayer.java
+++ src/main/java/com/thunderbear06/entity/player/AndroidPlayer.java
@@ -2,10 +2,8 @@ package com.thunderbear06.entity.player;
 
 import com.mojang.authlib.GameProfile;
 import com.thunderbear06.ai.AndroidBrain;
-import dan200.computercraft.shared.platform.PlatformHelper;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,21 +22,5 @@ public final class AndroidPlayer {
 
     public static GameProfile fallbackProfile(AndroidBrain brain) {
         return new GameProfile(UUID.nameUUIDFromBytes(brain.getAndroid().getUUID().toString().getBytes()), "Android");
-    }
-
-    @Nullable
-    public static ServerPlayer fake(AndroidBrain brain) {
-        if (!(brain.getAndroid().level() instanceof ServerLevel serverLevel)) return null;
-        GameProfile profile = brain.getOwningPlayerProfile() == null ? fallbackProfile(brain) : brain.getOwningPlayerProfile();
-        ServerPlayer player = PlatformHelper.get().createFakePlayer(serverLevel, profile);
-        player.moveTo(brain.getAndroid().getX(), brain.getAndroid().getY(), brain.getAndroid().getZ(), brain.getAndroid().getYRot(), brain.getAndroid().getXRot());
-        player.setItemInHand(InteractionHand.MAIN_HAND, brain.getAndroid().getMainHandItem().copy());
-        player.setItemInHand(InteractionHand.OFF_HAND, brain.getAndroid().getOffhandItem().copy());
-        return player;
-    }
-
-    public static void copyHandsBack(AndroidBrain brain, ServerPlayer player) {
-        brain.getAndroid().setItemInHand(InteractionHand.MAIN_HAND, player.getMainHandItem().copy());
-        brain.getAndroid().setItemInHand(InteractionHand.OFF_HAND, player.getOffhandItem().copy());
     }
 }
